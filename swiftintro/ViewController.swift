a4162c7629a80b589ffba87fd0424c848a3e407c
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-13 15:13:19 +0600

    Initial Commit

---
+++ swiftintro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftintro
+//
+//  Created by Bakyt Dzhumabaev on 13/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
