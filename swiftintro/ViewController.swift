3e431584778beec8c7e8b6b7add6bc25faa77d6f
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-13 17:03:02 +0600

    weak connection label, button and textview

--- swiftintro/ViewController.swift
+++ swiftintro/ViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButtonClicked: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("finally github connected to my project")
+        
         
     }
 
