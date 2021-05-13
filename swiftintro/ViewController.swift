e7490eddd069b3e15218722e23965b4e67247b96
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-13 15:33:08 +0600

    init commit

--- swiftintro/ViewController.swift
+++ swiftintro/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("finally github connected to my project")
+        
     }
 
 
